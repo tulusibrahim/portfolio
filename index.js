b3c2c8c18fec4576ba70f3e2f30ba1d43ef55b2c
tulusibrahim <tulusibrahim20@gmail.com>
2021-01-28 21:03:07 +0700

    again

---
+++ index.js
@@ -0,0 +1,33 @@
+let btngo = document.getElementById('btngo')
+let option = document.getElementById('option')
+let overlay = document.getElementById('overlay')
+let nav = document.getElementById('nav')
+let backtotop = document.getElementById('backtotop')
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        gsap.to("#nav", { y: -100, duration: .3, ease: 'power1.out' })
+    } else {
+        gsap.to("#nav", { y: 0, duration: .3, ease: 'power1.out' })
+    }
+
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        backtotop.style.opacity = '1'
+    } else {
+        backtotop.style.opacity = '0'
+    }
+}
+
+backtotop.addEventListener('click', function () {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+})
+
+btngo.addEventListener('click', function () {
+    let tl = gsap.timeline()
+    tl.to('#btnword', { y: 100, duration: 1, ease: 'power4.in' })
+        .to('#btnword', { y: 0, duration: 1, ease: 'power4.in', delay: 1 })
+    setTimeout(() => {
+        window.location.href = '#wrapperproject'
+    }, 1100);
+})
